adc9c7722c24e977fcc89b2a94a8ea5dd4b61798
TulupaAnton <tulupaanton2@gmail.com>
2024-01-16 17:56:59 +0200

    Add user migration

---
+++ migrations/20240116154057-create-user.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      passw_hash: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      birthday: {
+        type: Sequelize.DATEONLY,
+      },
+      gender: {
+        type: Sequelize.ENUM('male', 'female', 'other'),
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
