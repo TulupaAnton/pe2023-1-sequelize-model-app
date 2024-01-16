4d31c60e02d59361b389e175183c826a7659ece7
TulupaAnton <tulupaanton2@gmail.com>
2024-01-16 18:23:08 +0200

    Add birthday constrains

---
+++ migrations/20240116161150-add-borthday-constraint.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { Op } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('users', {
+      fields: ['birthday'],
+      type: 'check',
+      name: 'birthday_check',
+      where: {
+        birthday: {
+          [Op.lte]: Sequelize.literal('CURRENT_DATE'),
+        },
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('users');
+  },
+};
