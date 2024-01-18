16a8436a234cbece590051f05076eb021975faec
TulupaAnton <tulupaanton2@gmail.com>
2024-01-18 07:14:22 +0200

    Add passwHash

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,32 @@
-module.exports.createUsers = async (req, res, next) => {};
+const { hashSync } = require('bcrypt');
+const { User } = require('./../models');
+
+// // Шиифрування
+// "123"->'fgfsgaglkdglakslkdwsl'
+// "123"<-'fgfsgaglkdglakslkdwsl'
+// // хешуванняя
+// "123"->"adslfaksflskfalfkaslf"
+
+// const passw = '12345';
+const HASH_SALT = 10;
+// const passwHash = hashSync(passw, HASH_SALT);
+// console.log('passwHash :>> ', passwHash);
+
+module.exports.createUsers = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    body.passwHash = hashSync(body.passwHash, HASH_SALT);
+
+    const crestedUser = await User.create(body);
+    if (!crestedUser) {
+      return res.status(400).send('Somthing went wrong');
+    }
+    res.status(201).send(crestedUser);
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.getUsers = async (req, res, next) => {};
 
